9b026328f761d122f2b29a87ad999e451185e967
Léo <leomarchal.dev@gmail.com>
2023-06-02 16:01:30 +0200

    🩹 fix XSS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const params = new URLSearchParams(window.location.search);
-const titleElement = document.getElementById('title');
-const name = params.get('name');
-titleElement.innerHTML = `Hello ${name ?? ''}`;
+// const params = new URLSearchParams(window.location.search);
+// const titleElement = document.getElementById('title');
+// const name = params.get('name');
+// titleElement.innerHTML = `Hello ${name ?? ''}`;
